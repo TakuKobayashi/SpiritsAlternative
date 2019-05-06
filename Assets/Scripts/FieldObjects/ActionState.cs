720e30ae9146a90a4d8f9f803982ab9232869ee6
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-05-06 21:13:32 +0900

    移動できる仕組みを形成

---
+++ Assets/Scripts/FieldObjects/ActionState.cs
@@ -0,0 +1,7 @@
+﻿using System;
+public enum ActionState{
+  Idle,
+  Targeting,
+  Moving,
+  Attack,
+}
--- Assets/Scripts/FieldObjects/MovableObject.cs
+++ Assets/Scripts/FieldObjects/MovableObject.cs
@@ -4,21 +4,55 @@ using System.Collections;
 public abstract class MovableObject : FieldObject
 {
     protected Vector3 targetPosition = Vector3.zero;
+    protected ActionState State = ActionState.Idle;
+    [SerializeField] protected float Speed = 1f;
 
-    public void TargetTo(Vector3 targetTo)
+    public virtual void TargetTo(Vector3 targetTo)
     {
         targetPosition = targetTo;
+        this.transform.LookAt(targetPosition);
+        State = ActionState.Targeting;
     }
 
-    protected virtual IEnumerator MoveTo(float reachedTime)
+    protected virtual IEnumerator MoveRoutine()
     {
         Vector3 beforePosition = this.transform.position;
+        float reachedTime = CalcReachedSecond();
         float second = 0;
-        while (second < reachedTime)
+        while (true)
         {
-            this.transform.position = Vector3.Lerp(beforePosition, targetPosition, second / reachedTime);
-            second += Time.deltaTime;
+            if(State == ActionState.Targeting)
+            {
+                beforePosition = this.transform.position;
+                reachedTime = CalcReachedSecond();
+                second = 0f;
+                State = ActionState.Moving;
+            }
+            if(State == ActionState.Moving)
+            {
+                this.transform.position = Vector3.Lerp(beforePosition, targetPosition, second / reachedTime);
+                second += Time.deltaTime;
+                if(second >= reachedTime)
+                {
+                    this.transform.position = targetPosition;
+                    second = reachedTime;
+                    State = ActionState.Idle;
+                }
+            }
             yield return null;
+        }
+    }
+
+    protected virtual float CalcReachedSecond()
+    {
+        Vector3 diff = this.transform.position - targetPosition;
+        if(Speed == 0f)
+        {
+            return 0f;
+        }
+        else
+        {
+            return diff.sqrMagnitude / Speed;
         }
     }
 }
